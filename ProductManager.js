8c5b8a3ee05e793b8d5bfa4fef0b0e3fa5ee2acf
Yerson <barrioslafont.5@gmail.com>
2023-07-18 23:45:58 -0500

    desafio2

--- ProductManager.js
+++ ProductManager.js
@@ -1,22 +1,56 @@
+const fs = require("fs");
+
 class ProductManager {
     constructor() {
         this.products = [];
+        this.path = "Products.json";
+        this.createFile();
+    }
+
+    createFile() {
+        if (!fs.existsSync(this.path)) {
+            fs.writeFileSync(this.path, JSON.stringify(this.products))
+        }
     }
 
     addProduct(product) {
         if (this.validateCode(product.code)) {
             console.log("Error! file CODE exists already!");
         } else {
-            const producto = {id:this.generateId(), title:product.title, description:product.description, price:product.price, thumbnail:product.thumbnail, code:product.code, stock:product.stock};
+            const producto = { id: this.generateId(), title: product.title, description: product.description, price: product.price, thumbnail: product.thumbnail, code: product.code, stock: product.stock };
+            this.products = JSON.parse(fs.readFileSync(this.path, "utf-8"));
             this.products.push(producto);
-            console.log("product added!")
-    }}
+            fs.writeFileSync(this.path, JSON.stringify(this.products));
+            console.log("product added!");
+        }
+    }
+
+    updateProduct(id, product) {
+
+    }
+
+    deleteProduct(id) {
+        this.products = JSON.parse(fs.readFileSync(this.path, "utf-8"));
+        let pos = this.products.findIndex(item => item.id === id);
+
+        if (pos > -1) {
+            this.products.splice(pos, 1);
+            fs.writeFileSync(this.path, JSON.stringify(this.products));
+            console.log("Product #" + id + " deleted!");
+        } else {
+            console.log("Not found");
+        }
+
+    }
 
     getProducts() {
+        this.products = JSON.parse(fs.readFileSync(this.path, "utf-8"));
+
         return this.products;
     }
 
     getProductById(id) {
+        this.products = JSON.parse(fs.readFileSync(this.path, "utf-8"));
         return this.products.find(item => item.id === id) || "Not found";
     }
 
@@ -34,16 +68,19 @@ class ProductManager {
         });
 
         return max+1;*/
-        return this.products.length > 0 ?this.products[this.products.length-1].id+1 : 1;
+        return this.products.length > 0 ? this.products[this.products.length - 1].id + 1 : 1;
     }
 
 }
 
 const PM = new ProductManager();
+//console.log(PM.getProducts());
+/*PM.addProduct({ title: "Curso Front-end Online", description: "Modalidad a tu ritmo", price: 500000, thumbnail: "No image", code: "Front-end", stock: 25 });
 console.log(PM.getProducts());
-PM.addProduct({title:"Curso Front-end Online", description:"Modalidad a tu ritmo", price:500000, thumbnail:"No image", code:"Front-end", stock:25});
-PM.addProduct({title:"Curso Front-end Online", description:"Modalidad a tu ritmo", price:500000, thumbnail:"No image", code:"Front-end", stock:50});
-PM.addProduct({title:"Curso Back-end-", description:"Modalidad a tu ritmo", price:500000, thumbnail:"No image", code:"Back-end", stock:100});
+PM.addProduct({ title: "Curso Front-end Online", description: "Modalidad a tu ritmo", price: 500000, thumbnail: "No image", code: "Front-end", stock: 50 });
+PM.addProduct({ title: "Curso Back-end-", description: "Modalidad a tu ritmo", price: 500000, thumbnail: "No image", code: "Back-end", stock: 100 });
+*/
+//console.log(PM.getProductById(3));
+//console.log(PM.getProductById(1));
+PM.deleteProduct(2);
 console.log(PM.getProducts());
-console.log(PM.getProductById(2));
-console.log(PM.getProductById(3));
